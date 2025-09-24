a2df9dcea4adb8efdf8880a60b4131dd17c0977b
pwonders-801 <pwonders@charlotte.edu>
2025-09-24 19:56:37 -0400

    added calculator functions
    added add, subtract, multiply, and divide to the calculator class.

--- good-calculator.py
+++ good-calculator.py
@@ -6,6 +6,16 @@ Coding Principles Demonstrated:
 3. Single responsibility
 """
 class Calculator:
-
+  def add(self, x, y):
+    return x + y
+  def subtract(self, x, y):
+    return x - y
+  def multiply(self, x, y):
+    return x * y
+  def divide(self x, y):
+    if y == 0:
+      raise ValueError("You cannot divide by 0.")
+    return x / y
+    
 def main(): 
   
