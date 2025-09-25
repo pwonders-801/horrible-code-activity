4883206e41bc0b5173ccbbcf945e1aa208a2a3be
pwonders-801 <pwonders@charlotte.edu>
2025-09-24 20:11:04 -0400

    added main function
    added user input handling, function call, output, and error handling in the main function.

--- good-calculator.py
+++ good-calculator.py
@@ -18,4 +18,33 @@ class Calculator:
     return x / y
     
 def main(): 
-  
+  calc = Calculator()
+
+while True:
+  try:
+    x = float(input("Enter the first number: "))
+    y = float(input("Enter the second number: "))
+    operator = input("Options:\n+ for addition\n- for subtraction\n* for multiplication\n"
+                      "/ for division\nexit to quit\nEnter a command: ")
+    if operator == "exit":
+      print("You have exited the program.")
+      break
+
+    operations = {
+      "+": calc.add,
+      "-": calc.subtract,
+      "*": calc.multiply,
+      "/": calc.divide
+    }
+
+    if operator in operations:
+      result = operations[operator](x, y)
+      print(f"Result: {result}\n")
+    else:
+      print("Invalid operator.\n")
+  except ValueError as e:
+    print(f"Error: {e}\n")
+
+
+if __name__ == "__main__":
+  main()
